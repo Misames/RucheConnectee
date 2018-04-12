b24a285455653fc763cee9bc6ecf6a58fe87ea8e
François <francois.bouscaillou@gmail.com>
2018-04-12 19:02:09 +0200

    Script de lecture et parse le texte

--- script/testRead.py
+++ script/testRead.py
@@ -5,9 +5,11 @@ import serial
 import sys
 import time
 
-ser = serial.Serial( port='/dev/ttyACM0', baudrate = 9600)
-while 1:
- x = ser.readline()
- print(x)
- if x == "H =":
-     humi = ''
+# ser = serial.Serial( port='/dev/ttyACM0', baudrate = 9600)
+
+fichier = open('test.txt',"r") # tu met un fichier txt au même endoirt que le script  avec le nom aa.txt
+for ligne in fichier:
+    if "PCI Location ID" in ligne:
+        line = ligne.split()
+        print("PCI Location ID :",line[-1])
+        var = line[-1]
