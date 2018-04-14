b8bab631611e9ba325ce429f3d7283548d36d35f
François <francois.bouscaillou@gmail.com>
2018-04-14 18:45:25 +0200

    correction des commentaires

--- script/ReadAndSendRequestSQL.py
+++ script/ReadAndSendRequestSQL.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
 
-# on import les fonctionnalités et bibliothèques qu'on n'a besoin pour traiter les informations
+# on import les fonctionnalités et bibliothèques dont on a besoin pour traiter les informations
 
 import serial
 import sys
 import time
 import pymysql.cursors
 
-# Connexion à la base de donnés
+# Connexion à la base de données
 
 connection = pymysql.connect(host='localhost',
                              user='root',
@@ -34,14 +34,14 @@ humi_max = 00.00
 compteur_serial = 0
 compteur = 0
 
-date = time.strftime("%d %H") # fonction pour avoirs le jour actuel et l'heure
+date = time.strftime("%d %H") # fonction pour avoir le jour actuel et l'heure
 ser = serial.Serial( port='/dev/ttyACM0', baudrate = 9600) # fonction pour lire le port série
 
 try:
     with connection.cursor() as cursor:
     
         # création de la table 
-        print("Verification ... faite a " , date)
+        print("Verification ... fait a " , date)
         time.sleep(3)
         sql = "CREATE TABLE IF NOT EXISTS `Ruche` (`id` int(255) NOT NULL AUTO_INCREMENT, `nb_abeille` int(255) COLLATE utf8_bin NOT NULL,`mass_miel` float(22) COLLATE utf8_bin NOT NULL,`message_alert` int(255) COLLATE utf8_bin NOT NULL, `temp_int` float(22) COLLATE utf8_bin NOT NULL, `temp_ext` float(22) COLLATE utf8_bin NOT NULL,`temp_min` float(22) COLLATE utf8_bin NOT NULL,`temp_max` float(22) COLLATE utf8_bin NOT NULL,`humi_int` float(22) COLLATE utf8_bin NOT NULL, `humi_ext` float(22) COLLATE utf8_bin NOT NULL,`humi_min` float(22) COLLATE utf8_bin NOT NULL,`humi_max` float(22) COLLATE utf8_bin NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1"
         cursor.execute(sql)
@@ -49,7 +49,7 @@ try:
     connection.commit() #Commit
 
 finally:
-    print('Verification terminer.')
+    print('Verification terminee.')
 
 while 1:
     while compteur_serial == 0:
@@ -66,13 +66,13 @@ while 1:
                     line = ligne.split()
                     print("alert =",line[-1])
                     message_alert = line[-1]
-                if "temperature interieur" in ligne: 
+                if "temperature interieure" in ligne: 
                     line = ligne.split()
-                    print("temperature interieur =",line[-2])
+                    print("temperature interieure =",line[-2])
                     temp_int = line[-2]
-                if "temperature exterieur" in ligne: 
+                if "temperature exterieure" in ligne: 
                     line = ligne.split()
-                    print("temperature exterieur =",line[-2])
+                    print("temperature exterieure =",line[-2])
                     temp_ext = line[-2]
                 if "temperature minimum" in ligne: 
                     line = ligne.split()
@@ -82,21 +82,21 @@ while 1:
                     line = ligne.split()
                     print("temperature maximum =",line[-2])
                     temp_max = line[-2]
-                if "humiditer interieur" in ligne: 
+                if "humidite interieure" in ligne: 
                     line = ligne.split()
-                    print("humiditer interieur =",line[-2])
+                    print("humidite interieure =",line[-2])
                     humi_int = line[-2]
-                if "humiditer exterieur" in ligne: 
+                if "humidite exterieure" in ligne: 
                     line = ligne.split()
-                    print("humiditer exterieur =",line[-2])
+                    print("humidite exterieure =",line[-2])
                     humi_ext = line[-2]
-                if "humiditer minimum" in ligne: 
+                if "humidite minimum" in ligne: 
                     line = ligne.split()
                     print("humiditer minimum =",line[-2])
                     humi_min = line[-2]
-                if "humiditer maximum" in ligne: 
+                if "humidite maximum" in ligne: 
                     line = ligne.split()
-                    print("humiditer maximum =",line[-2])
+                    print("humidite maximum =",line[-2])
                     humi_max = line[-2]
                 if "fini" in ligne: 
                     compteur_serial = 1
@@ -105,16 +105,16 @@ while 1:
                     date = time.strftime("%d %H")
                     dateComplete = time.strftime("%A %d %B %Y %H:%M:%S")
                     
-                    if date == '31 02':   # reset de la base de donnée le 31 du mois à 2h du matin et pendant une heure bloc le système
+                    if date == '31 02':   # reset de la base de données le 31 du mois à 2h du matin et pendant une heure bloc le système
                         sql = "TRUNCATE TABLE `Ruche`"
                         cursor.execute(sql) #Execution
-                        print("Table purger a ", date)
+                        print("Table purgee a ", date)
                         print("Chargement de la nouvelle table ...")
                         time.sleep(3600)
                         compteur = 0
                         connection.commit() #Commit
                         
-                    # On execute une requêtes SQL pour ajouter des données à la BDD
+                    # On execute une requête SQL pour ajouter des données à la BDD
                     sql = "INSERT INTO `Ruche` (`nb_abeille`, `mass_miel`, `message_alert`,  `temp_int`, `temp_ext`, `temp_min`, `temp_max` , `humi_int`, `humi_ext`, `humi_min`, `humi_max`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s ,%s, %s, %s)" # requête SQL pour mettre à jour la base de données après la Création et purge de celci juste avant si la condition est vérifé
                     cursor.execute(sql, (nb_abeille, mass_miel, message_alert, temp_int, temp_ext, temp_min, temp_max, humi_int, humi_ext, humi_min, humi_max)) #Execution
                     connection.commit() #Commit
